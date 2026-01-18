4877aff2f65364818fd931040cdcb32dc3029ecc
GraphicHippo <jenzenauer@gmail.com>
2026-01-18 17:43:53 -0600

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -296,8 +296,11 @@
         <li>6 points per punt or kick return TD</li>
     </ul>
 
-    <h3 bind:this={fourFive}>4.5 Decisions</h3>
+    <h3 bind:this={fourFive}>4.5 Divisions</h3>
     <p>Each week players will go head to head, matchups randomly determined at the beginning of the season. Each team will play for an additional win by comparing their score against the league scoring average for the week.</p>
+
+    <h3 bind:this={fourSix>4.6 Tiebreakers</h3>
+    <p>Matchup Tiebreaker rules will be as follows: Highest total points for (including all bench players), then a coin flip.</p>
 
     <h2 class="sectionHeading" bind:this={five}>Section 5 Tanking Policy</h2>
     
