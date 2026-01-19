16ea6ef6ac88017dd189b124a2bfea91412581e4
GraphicHippo <jenzenauer@gmail.com>
2026-01-18 18:01:16 -0600

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -306,8 +306,8 @@
 
     <h2 class="sectionHeading" bind:this={five}>Section 5 Waivers/Trades</h2>
     
-    <h3 bind:this={fiveOne}>5.1 League Tanking Policy</h3>
-    <p>No team may intentionally leave starting roster spots empty. Any evidence of intentionally leaving roster spots empty in order to improve your draft position may be subject to punishment, including but not limited to the forfeiture of draft selections.</p>
+    <h3 bind:this={fiveOne}>5.1 Free Agents</h3>
+    <p>Free agency will be handled with FAAB (free agency acquisition budget). Each team will have $100 for in-season free agency acquisitions, this budget is season-long and will not reset each week. The team who bids the most on each free agent will win the bid. FAAB will reset for the off-season and each team will have $100 that expires at the start of the league year.</p>
 
     <h2 class="sectionHeading" bind:this={six}>Section 6 Replacing Managers</h2>
     
